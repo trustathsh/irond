8fe0f5639d784df7d4ab615fc620c02b0d103bf8
Ralf Steuerwald <ralf.steuerwald@hs-hannover.de>
2014-08-26 12:44:51 +0200

    build: update travis config for unmanaged dependency

--- src/main/templates/installation/mvn-install-sunxacml.sh
+++ src/main/templates/installation/mvn-install-sunxacml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn install:install-file -Dfile=../lib/sunxacml-2.0-M2-SNAPSHOT.jar \
+mvn install:install-file -Dfile=$(dirname $0)/../lib/sunxacml-2.0-M2-SNAPSHOT.jar \
 			   -DgroupId=sunxacml \
 			   -DartifactId=sunxacml \
 			   -Dversion=2.0-M2 \
