eef3e28a0d81f03bcf60167fdfc7ce3544a00a07
Bastian Hellmann <bahellma@googlemail.com>
2015-06-29 12:21:20 +0200

    [release] version 0.5.4

--- src/main/java/de/hshannover/f4/trust/iron/mapserver/binding/FilterFactory.java
+++ src/main/java/de/hshannover/f4/trust/iron/mapserver/binding/FilterFactory.java
@@ -7,18 +7,18 @@
  *    | | | |  | |_| \__ \ |_| | (_| |  _  |\__ \|  _  |
  *    |_| |_|   \__,_|___/\__|\ \__,_|_| |_||___/|_| |_|
  *                             \____/
- *
+ * 
  * =====================================================
- *
+ * 
  * Hochschule Hannover
  * (University of Applied Sciences and Arts, Hannover)
  * Faculty IV, Dept. of Computer Science
  * Ricklinger Stadtweg 118, 30459 Hannover, Germany
- *
+ * 
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de/
- *
- * This file is part of irond, version 0.5.3, implemented by the Trust@HsH
+ * 
+ * This file is part of irond, version 0.5.4, implemented by the Trust@HsH
  * research group at the Hochschule Hannover.
  * %%
  * Copyright (C) 2010 - 2015 Trust@HsH
@@ -26,9 +26,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
